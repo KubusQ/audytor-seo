ae8981db121cc04ef0a7c8af0aa6b7d89be60a8d
Kubus <kubus.bandola@gmail.com>
2023-08-19 16:16:21 +0200

    added login controller and template added registration controller template modyfications

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'The password fields must match.',
+                'attr' => ['autocomplete' => 'new-password'],
+                'first_options' => [
+                    'label' => 'Hasło',
+                    'mapped' => false,
+                ],
+                'second_options' => [
+                    'label' => 'Powtórz hasło',
+                    'mapped' => false,
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Proszę wprowadzić hasło',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Hasło powinno mieć minimum {{ limit }} znaków',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
